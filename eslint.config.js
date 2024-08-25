9b783562788cc1cd5bf6419e2dd0131e1d88dcfa
Slyther Savior <shrijanpoudel9@gmail.com>
2024-08-25 18:24:01 +0545

    eslint , prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,32 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import pluginJs from '@eslint/js'
+import standard from 'eslint-config-standard'
+import pluginN from 'eslint-plugin-n'
+import pluginPromise from 'eslint-plugin-promise'
+import pluginImport from 'eslint-plugin-import'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+const config = [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+      globals: {
+        ...globals.mocha, // I am using mocha, remove if not needed...
+        ...globals.node,
+      }
+    }
   },
-)
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+      n: pluginN,
+      import: pluginImport,
+      promise: pluginPromise
+    },
+    rules: standard.rules
+  },
+  {
+    ignores: ['coverage/', 'dist/', 'lib/']
+  }
+]
+
+export default config
